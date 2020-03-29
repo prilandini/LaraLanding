843c8753ab4c4c111a4a4932e94c04ece2b80824
Prilandini <prillyandininurul@gmail.com>
2020-03-29 21:03:10 +0700

    fix admin view

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -12,9 +12,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <title>AdminLTE 3 | Starter</title>
 
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="{{asset('Ite/plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{asset('lte/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('Ite/dist/css/adminlte.min.css')}}">
+  <link rel="stylesheet" href="{{asset('lte/dist/css/adminlte.min.css')}}">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
@@ -77,10 +77,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
-<script src="{{asset('Ite/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('lte/plugins/jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap 4 -->
-<script src="{{asset('Ite/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('lte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{asset('Ite/dist/js/adminlte.min.js')}}"></script>
+<script src="{{asset('lte/dist/js/adminlte.min.js')}}"></script>
 </body>
 </html>
